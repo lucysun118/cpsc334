88367ada545ce6a5b522ad1c80343e95b7bc6dbe
lucysun118 <l.sun@yale.edu>
2023-10-03 20:44:15 -0400

    fixed string processing

--- raspberrypi/labs/lb4/esp32_arduino.py
+++ raspberrypi/labs/lb4/esp32_arduino.py
@@ -3,5 +3,6 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 
 while True:
-	receivedString = ser.readline()
+	receivedString = ser.readline()[:-2]
+	receivedString = str(receivedString,'utf-8')
 	print(receivedString)
